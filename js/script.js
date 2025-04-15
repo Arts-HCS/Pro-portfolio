1ae1be517656b1cce3d87471486d70d149c3e2a6
Arturo Sandoval <artssc925@gmail.com>
2025-04-15 13:12:52 -0600

    Cambios en el botón de idiomas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const nav = document.getElementById('nav');
 const abrir = document.getElementById('abrir')
 const botonesCerrar = document.querySelectorAll('.cerrar');
+const btn = document.getElementById('translate-btn');
+
+btn.addEventListener('click', () => {
+  btn.classList.toggle('active');
+});
 
 abrir.addEventListener('click', () =>{
     nav.classList.add("visible")
